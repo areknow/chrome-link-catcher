5ee8d5e1d97c4b9c3b20ed7599a2b3d278e3c310
Arnaud Crowther <arnaudcrowther@gmail.com>
2014-12-02 20:56:00 -0500

    Testing javascript scraping methods

--- popup.js
+++ popup.js
@@ -4,8 +4,6 @@ window.addEventListener('click',function(e){
         chrome.tabs.create({url:e.target.href})
     }
 })
-
-
 // pass urls to php script get links back
 function getLinks(url) {
     var data = {URL: url};
@@ -15,10 +13,53 @@ function getLinks(url) {
 }
 
 
-// after load: pass tab url to getLinks()
+
+
+
+function paintDiv(text) {
+    $('#link-container').html(text);
+}
+function DOMtoString(document_root) {
+    var html = '',
+        node = document_root.firstChild;
+    while (node) {
+        switch (node.nodeType) {
+        case Node.ELEMENT_NODE:
+            html += node.outerHTML;
+            break;
+        case Node.TEXT_NODE:
+            html += node.nodeValue;
+            break;
+        case Node.CDATA_SECTION_NODE:
+            html += '<![CDATA[' + node.nodeValue + ']]>';
+            break;
+        case Node.COMMENT_NODE:
+            html += '<!--' + node.nodeValue + '-->';
+            break;
+        case Node.DOCUMENT_TYPE_NODE:
+            // (X)HTML documents are identified by public identifiers
+            html += "<!DOCTYPE " + node.name + (node.publicId ? ' PUBLIC "' + node.publicId + '"' : '') + (!node.publicId && node.systemId ? ' SYSTEM' : '') + (node.systemId ? ' "' + node.systemId + '"' : '') + '>\n';
+            break;
+        }
+        node = node.nextSibling;
+    }
+    return html;
+}
+
+
 $(document).ready(function() {
     chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-        var url = tabs[0].url;
-//        getLinks(url);
+        var link = tabs[0].url;
+        
+        
+//        var links = ["test1","test2","test3","test4"];
+//        var stack ="";
+//        jQuery.each(links, function(i, val) {
+//            stack += val + "<br />";
+//        });
+//        
+//        paintDiv(stack);
+        
+        DOMtoString(document);
     });
 });
